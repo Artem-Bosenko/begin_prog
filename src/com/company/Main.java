dd91e2b5ba9bd77f0a371dc42ceb89228fa771c0
Босенко Артем <artem.bosenko@nure.ua>
2021-02-12 22:22:45 +0200

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,10 @@
 package com.company;
-import org.jetbrains.annotations.NotNull;
-
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.util.*;
 
 public class Main implements IsSampleInterface {
-    static int c2 = 20;
 
     public static void main(String[] args) {
         String b = "B204B";
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
+
         int f = resolve(b);
         System.out.println(f);
 
@@ -22,11 +15,12 @@ public class Main implements IsSampleInterface {
         String b = "ABCEHKMOPTXY";
         String c = "0123456789";
         boolean isTrue = true;
-        for (int i = 0; i< mas.length; i++) {
-            boolean t = false;
-            if(i == 0 || i== 4 || i==5) {
+        for (int i = 0; i < mas.length; i++) {
+            boolean t;
+
+            if (i == 0 || i == 4 || i == 5) {
                 t = b.contains(mas[i]);
-                if(t == false) {
+                if(!t) {
                     isTrue = false;
                     break;
                 }
@@ -34,13 +28,13 @@ public class Main implements IsSampleInterface {
             }
             else {
                 t = c.contains(mas[i]);
-                if(t == false) {
+                if(!t) {
                     isTrue = false;
                     break;
                 }
             }
         }
-        if(isTrue == true) return 100;
+        if(isTrue) return 100;
         else return mas.length;
     }
 
